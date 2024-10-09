d54e0ccb540d1a533c915cd070a5c54ca79025e8
Eduardo Alvarado <ealvara73@gmail.com>
2024-10-08 21:37:59 -0500

    Create project skeleton

---
+++ app/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.send('this is the home page.');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => { console.log(`Express app listening on port ${PORT}`)});
