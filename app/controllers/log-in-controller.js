fa3dfe394f3e536ebb3392fceb2665a7e191fbfc
Eduardo Alvarado <ealvara73@gmail.com>
2024-10-28 20:49:41 -0500

    Add log in validation

--- app/controllers/log-in-controller.js
+++ app/controllers/log-in-controller.js
@@ -1,10 +1,16 @@
 const passport = require('../passport');
 
 exports.getLogIn = async(req, res) => {
-    res.render('log-in');
+    if(req.session.messages){
+        const errMessage = req.session.messages;
+        const size = Object.keys(errMessage).length;
+        console.log(size);
+        if(size > 1) errMessage.shift();
+    }
+    res.render('log-in', {error: req.session.messages });
 };
 
 exports.LogInUser = passport.authenticate('local', {
     successRedirect: '/',
-    failureRedirect: '/log-in'
+    failureRedirect: '/log-in', failureMessage: true
 });
